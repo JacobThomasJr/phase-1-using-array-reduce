85f734e1d6da095df13082180f9ac0df18c2a72d
jacobaranza <jacobthomasjunior@gmail.com>
2023-04-26 10:10:23 -0500

    array reduce lab

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-// Code your solution here
+// Calculate the sum of all battery amounts using reduce()
+const totalBatteries = batteryBatches.reduce((acc, curr) => acc + curr, 0);
+
+console.log(totalBatteries); // Output: 31
+
